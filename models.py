141cbf4fffa24ee13b5c06d34db56ea57f5ebdfc
tbtreppe <63826526+tbtreppe@users.noreply.github.com>
2021-11-11 18:02:04 -0500

    Add files via upload

--- models.py
+++ models.py
@@ -2,18 +2,18 @@
 from flask_sqlalchemy import SQLAlchemy 
 db = SQLAlchemy()
 
-    class User(db.Model):
-        __tablename__= "users"
+class User(db.Model):
+     __tablename__= "users"
 
-        def __repr__(self):
-            u= self
-            return f"<user id = {u.id} first_name = {u.first_name} last_name= {u.last_name} image_url= {u.image_url}>"
+    def __repr__(self):
+        u= self
+        return f"<user id = {u.id} first_name = {u.first_name} last_name= {u.last_name} image_url= {u.image_url}>"
         
-        id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-        first_name = db.Column(db.text, nullable = False, unique= True)
-        last_name = db.Column(db.text, nullable = False, unique= True)
-        image_url =  db.Column(db.text, unique = True)
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    first_name = db.Column(db.Text, nullable=False, unique= True)
+    last_name = db.Column(db.Text, nullable=False, unique= True)
+    image_url = db.Column(db.Text, nullable=False, unique= True)
 
-    def connect_db(app):
+def connect_db(app):
     db.app=app
     db.init_app(app)
