63c8097132c3d42de74f38f010269bf702f21d4f
tbtreppe <63826526+tbtreppe@users.noreply.github.com>
2021-11-17 22:05:05 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, render_template, request, redirect
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
@@ -62,6 +62,48 @@ def delete_user(user_id):
    
     user = User.query.get_or_404(user_id)
     db.session.delete(user)
+    db.session.commit()
+
+    return redirect("/user")
+
+@app.route('/user/<int:user_id>/new_post')
+def show_new_post_form(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('new_post.html', user=user)
+
+@app.route('/user/<int:user_id>/new_post', methods=["POST"])
+def add_new_post(user_id):
+    user = User.query.get_or_404(user_id)
+    new_post = Post(
+        title=request.form['title'],
+        content=request.form['content'])
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect("/detail")
+
+@app.route('/post/<int:post_id>')
+def show_post_details(post_id):
+    post = Post.query.get_or_404(user_id)
+    return render_template('post_detail.html', post=post)
+
+@app.route('/post/<int:post_id>/edit', methods=["POST"])
+def edit_post():
+    post = Post.query.get_or_404(post_id)
+    title=request.form['title'],
+    content=request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect("/user")
+
+@app.route('/post/<int:post_id>/delete', methods=["POST"])
+def delete_post(post_id):
+   
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
     db.session.commit()
 
     return redirect("/user")
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 """Models for Blogly."""
+import datetime
 from flask_sqlalchemy import SQLAlchemy 
 db = SQLAlchemy()
 
@@ -11,7 +12,22 @@ class User(db.Model):
     first_name = db.Column(db.Text, nullable=False)
     last_name = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.Text, nullable=False)
+    post=db.relationship("Post", backref="user")
+
+
+
+class Post(db.Model):
+    __tablename__="post"
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.Text, nullable=False, unique=True)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default= datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
 
 def connect_db(app):
     db.app=app
-    db.init_app(app)
+    db.init_app(app)    
+
+
