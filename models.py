49d570bbc3da0bd08f3f360b06d16dcf4264ee58
tbtreppe <63826526+tbtreppe@users.noreply.github.com>
2021-11-22 17:49:27 -0500

    Add files via upload

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class User(db.Model):
     first_name = db.Column(db.Text, nullable=False)
     last_name = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.Text, nullable=False)
-    post=db.relationship("Post", backref="user")
+    post = db.relationship("Post", backref="user")
 
 
 
@@ -24,6 +24,21 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default= datetime.datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+class PostTag(db.Model):
+    __tablename__="post_tag"
+
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'), primary_key=True)
+
+class Tag(db.Model):
+    __tablename__="tag"
+
+    id = db.Column(db.Integer, primary_key= True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+
+    post = db.relationship('Post', secondary = "post_tag", backref = "tag")
 
 
 def connect_db(app):
